347b418fb3a05eb4d383adc5cbb759961929ebd2
ishii-miki <m.ishii@ntt-ipnet.co.jp>
2022-06-28 14:51:14 +0900

    ishii up 0628

--- code/gekokujo_app/admin.py
+++ code/gekokujo_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ScoreTable
 
-# Register your models here.
+admin.site.site_header = '下剋上管理サイト'
+admin.site.index_title = 'スコアデータ'
+
+@admin.register(ScoreTable)
+class ScoreTableAdmin(admin.ModelAdmin):
+    pass
