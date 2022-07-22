be88a0bb32e4de3fb7b78f4b2df160100a88a770
nakazawaaoi <a.nakazawa@ntt-ipnet.co.jp>
2022-07-22 17:52:36 +0900

    nakazawa up

--- code/gekokujo_app/views/add_record_views.py
+++ code/gekokujo_app/views/add_record_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from ..models import ScoreTable
 from ..forms import RecordsForm
@@ -6,6 +6,8 @@ from ..forms import RecordsForm
 # 成績登録処理
 
 # formから取得したデータをDBに保存し、ランキング一覧に戻る
+
+
 def addRecord(request):
     if request.method == 'POST':
         recordsform = RecordsForm(request.POST)
