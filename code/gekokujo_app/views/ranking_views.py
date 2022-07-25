ee90c2e77f1a1eafd00f0a4d61dc7d7b7afdf142
nakazawaaoi <a.nakazawa@ntt-ipnet.co.jp>
2022-07-25 17:45:05 +0900

    nakazawa up

--- code/gekokujo_app/views/ranking_views.py
+++ code/gekokujo_app/views/ranking_views.py
@@ -26,14 +26,57 @@ RAW_SQL = """
 
 
 def showRecords(request):
-
-    records = ScoreTable.objects.filter(course='200').values('user_id').annotate(score_max=Max(
-        'score'), date_min=Min('date')).order_by('score_max').reverse().values(*["user_id", "score_max", 'date_min'])
-    courses = ScoreTable.objects.distinct().values_list('course')
-    context = {
-        'scoretable': records,
-        'course': courses
+    courses = {
+        100: 'おやじギャグ',
+        200: 'お嬢様ことば',
+        300: '早口ことば',
+        400: '回文',
+        500: '缶コーヒーのコピー',
+        600: '時代劇のセリフ',
+        700: '元気が出ることば',
+        800: 'アニメタイトル',
     }
+
+    records_query_set = ScoreTable.objects.filter(course='200').values('user_id').annotate(score_max=Max(
+        'score'), date_min=Min('date')).order_by('score_max').reverse().values(*["user_id", "score_max", 'date_min'])
+    selectcourses = ScoreTable.objects.all().order_by(
+        'course').distinct().values_list('course')
+
+    courselist = []
+    for n in selectcourses:
+        for l in str(n):
+            re.sub("[()]", "", l)
+            l.replace(",", "")
+            courselist.append(l)
+
+    coursedic = {}
+    for s in courselist:
+        for k, v in courses.items():
+            if k == s:
+                dic = {
+                    k: v
+                }
+                coursedic.update(dic)
+            else:
+                dic = {
+                    'test': 'aaaa'
+                }
+                coursedic.update(dic)
+    context = {
+        'scoretable': records_query_set,
+        'course': courselist
+    }
+
+    return render(request, 'records.html', context)
+
+
+# def unnecessary(x):
+#     courselist = []
+#     for n in x:
+#         n.replace(",","")
+#         re.sub("\(|\)", "", n)
+#         courselist.append(n)
+#     return courselist
 
 
 # def showRecords(request):
@@ -60,4 +103,25 @@ def showRecords(request):
     #     'scoretable': records
     # }
 
-    return render(request, 'records.html', context)
+    # print('---------')
+    # print(records_query_set)
+    # records = []
+    # scr = 0
+    # rank = 0
+    # same_rank = 0
+
+    # for record in records_query_set:
+    #     if scr != record.score_max:
+    #         rank += 1 + same_rank
+    #         scr = record.score_max
+    #     elif scr == record.score_max:
+    #         same_rank += 1
+    #     records.append(
+    #         {'rank': rank,
+    #          'name': record.name,
+    #          'rookie': True if record.user_id == int(settings.ROOKIES_ID) else False,
+    #          'level': record.level,
+    #          'score': record.score_max})
+    # context = {
+    #     'scoretable': records
+    # }
