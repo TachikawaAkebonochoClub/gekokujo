07af5c98d6ea837b5308c51b20b38eb29637ce95
ishii-miki <86756223+ishii-miki@users.noreply.github.com>
2022-07-27 19:04:51 +0900

    Update ranking_views.py

--- code/gekokujo_app/views/ranking_views.py
+++ code/gekokujo_app/views/ranking_views.py
@@ -6,7 +6,77 @@ from ..models import ScoreTable
 from django.conf import settings
 from django.db.models import Max, Min
 
-# ランキングを生成してrecords.htmlに返す
+################ コース選択用関数 ####################
+def ***():
+
+    courses = {
+        100: 'おやじギャグ',
+        200: 'お嬢様ことば',
+        300: '早口ことば',
+        400: '回文',
+        500: '缶コーヒーのコピー',
+        600: '時代劇のセリフ',
+        700: '元気が出ることば',
+        800: 'アニメタイトル',
+    }
+        
+    selectcourses = ScoreTable.objects.all().order_by(
+        'course').distinct().values_list('course')
+
+    courselist = []
+    for n in selectcourses:
+        for l in n:
+            courselist.append(l)
+
+    coursedic = {}
+    for s in courselist:
+        for k, v in courses.items():
+            if k == s:
+                dic = {
+                    k: v
+                }
+                coursedic.update(dic)
+    return coursedic
+
+# コースに応じて順位付けした結果を返す
+def showRecords(request):
+
+    if request.POST:
+        coursenum = request.POST["course"]
+        if coursenum == '0':
+            records_query_set = ScoreTable.objects.values('user_id').annotate(score_max=Max(
+                'score'), date_min=Min('date')).order_by('score_max').reverse().values(*["user_id", "score_max", 'date_min'])
+        else:
+            records_query_set = ScoreTable.objects.filter(course=coursenum).values('user_id').annotate(score_max=Max(
+                'score'), date_min=Min('date')).order_by('score_max').reverse().values(*["user_id", "score_max", 'date_min'])
+    else:
+        records_query_set = ScoreTable.objects.values('user_id').annotate(score_max=Max(
+            'score'), date_min=Min('date')).order_by('score_max').reverse().values(*["user_id", "score_max", 'date_min'])
+
+### 以下いじってないです
+    records = []
+    scr = 0
+    rank = 0
+    same_rank = 0
+
+    for record in records_query_set:
+        if scr != record.score_max:
+            rank += 1 + same_rank
+            scr = record.score_max
+        elif scr == record.score_max:
+            same_rank += 1
+        records.append(
+            {'rank': rank,
+             'name': record.name,
+             'rookie': True if record.user_id == int(settings.ROOKIES_ID) else False,
+             'level': record.level,
+             'score': record.score_max})
+    context = {
+        'scoretable': records,
+        'course': coursedic
+    }
+
+################### 以下以前のファイル ########################
 
 # RAW_SQL = """
 #           SELECT gekokujo_app_scoretable.* FROM gekokujo_app_scoretable,
